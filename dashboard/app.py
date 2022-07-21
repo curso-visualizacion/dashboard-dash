39541b3edf92bcb9a38f23ff241b22e04feea27a
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-07-20 22:27:19 -0400

    feat: histogram with dropdown

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
@@ -9,11 +9,33 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 path = "https://github.com/curso-visualizacion/practicas_diploma/blob/2022/Practica2/migrantes_chile.xlsx?raw=true"
 dataset = pd.read_excel(path)
 dataset.loc[:, "total"] = dataset.iloc[:, 4:].sum(axis=1)
+dataset.columns = dataset.columns.astype(str)
+dataset.set_index("Country", inplace=True)
+
+column_options = [{"label": col, "value": col} for col in dataset.columns[4:]]
 
 app.layout = dbc.Container(
-    dbc.Alert("Hello Bootstrap!", color="success"),
+    [
+        html.H1("Distribución"),
+        dbc.Select(id="year", options=column_options, value="total"),
+        dcc.Graph(id="histogram"),
+    ],
     className="p-5",
 )
+
+
+@app.callback(
+    Output(component_id="histogram", component_property="figure"),
+    Input(component_id="year", component_property="value"),
+)
+def update_histogram(input_value):
+    fig = go.Figure()
+    continents = dataset.Continent.unique()
+    for cont in continents:
+        fig.add_trace(
+            go.Histogram(x=dataset[dataset.Continent == cont][input_value], name=cont)
+        )
+    return fig
 
 
 if __name__ == "__main__":
